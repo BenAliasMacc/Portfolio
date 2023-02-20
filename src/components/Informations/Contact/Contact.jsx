741ae6dd80659c2acb0d15fec57ee7b2b26b0557
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2023-02-20 16:20:26 +0100

    form

--- src/components/Informations/Contact/Contact.jsx
+++ src/components/Informations/Contact/Contact.jsx
@@ -17,8 +17,8 @@ const Contact = () => {
                             Me contacter :
                         </span>
                     </h3>
-                    <form id='contact-form' method='post' data-netlify="true" action='/' onSubmit={(e) => e.preventDefault() } >
-                        <input type="hidden" name='form-name' value="contact-form" />
+                    <form name="contact" id='contact-form' method='post' data-netlify="true" action='/' onSubmit={(e) => e.preventDefault() } >
+                        <input type="hidden" name='form-name' value="contact" />
                         <div className='relative z-0 w-full mt-[40px] mb-8 group'>
                             <input name='username' type="text" className='block autofill:bg-transparent py-2.5 px-0 w-full text-sm text-gray-lite bg-transparent border-0 border-b-[2px] border-[#B5B5B5] appearance-none dark:text-white dark:border-[#333333] dark:focus:border-[#FF6464] focus:outline-none focus:ring-0 focus:border-[#FF6464] peer' />
                             <label htmlFor="" className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-color-910 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FF6464] peer-focus:dark:text-[#FF6464] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8'>Nom *</label>
